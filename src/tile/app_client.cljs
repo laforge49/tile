565d7387ad21f3b86881907ae261f4a407bccec6
Bill La Forge <laforge49@gmail.com>
2017-06-12 10:27:15 -0400

    basic tile component

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -4,34 +4,45 @@
             [reagent.core :as reagent :refer [atom]]
             [taoensso.sente :as sente :refer (cb-success?)]))
 
+(defn tile
+      [title content]
+      (fn [title content]
+          [:table
+           [:tbody
+            {:style {:border "5px solid red" :float "left"}}
+            [:tr
+             {:style {:background-color "yellow"}}
+             [:td
+              {:style {:padding "5px"}}
+              [:div
+               [:div
+                {:style {:float "left"}}
+                [:strong (str title " ")]]
+               [:div
+                {:style {:float "right"}}
+                "X"]]]]
+            [:tr
+             {:style {:background-color "Cornsilk"}}
+             [:td
+              {:style {:padding "5px"}}
+              (content)]]]]))
+
 (defn calling-component
       []
-      [:table
-       [:tbody
-        {:style {:border "5px solid red" :float "left"}}
-        [:tr
-         {:style {:background-color "yellow"}}
-         [:td
-          {:style {:padding "5px"}}
-          [:div
-          [:div
-           {:style {:float "left"}}
-           [:strong "Tile example"]]
-          [:div
-           {:style {:float "right"}}
-           "X"]]]]
-        [:tr
-         {:style {:background-color "Cornsilk"}}
-         [:td
-          {:style {:padding "5px"}}
-          [:p "try hitting the button:"]
-          [:p
-           [:input {:type "button"
-                    :value "chsk-send! (with reply)"
-                    :on-click (fn []
-                                  (->output! "Button 2 was clicked (will receive reply from server)")
-                                  (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
-                                              (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]]]]])
+      [:div
+       [tile
+        "hi"
+        (fn []
+            [:div
+             [:p "try hitting the button:"]
+             [:p
+              [:input {:type "button"
+                       :value "chsk-send! (with reply)"
+                       :on-click (fn []
+                                     (->output! "Button 2 was clicked (will receive reply from server)")
+                                     (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
+                                                 (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]])]
+       ])
 
 (defn start!
       []
