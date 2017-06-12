c5f494a927598ad0a35c1c6fc44ac72954d2a082
Bill La Forge <laforge49@gmail.com>
2017-06-12 12:33:14 -0400

    simplify

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -4,10 +4,12 @@
             [reagent.core :as reagent :refer [atom]]
             [taoensso.sente :as sente :refer (cb-success?)]))
 
+(def top-tile-atom (atom nil))
+
 (defn tile
       [state]
       (fn [state]
-          (let [{:keys [parent-tile-state display title content]} @state]
+          (let [{:keys [parent-tile-state child-tile-states display title content]} @state]
                (if (not display)
                  nil
                  [:table
@@ -33,26 +35,28 @@
                        nil)]]]]))))
 
 (defn basic-tile-state
-      [parent-tile-state title content]
-      (atom {:parent-tile-state parent-tile-state
-             :display true
-             :title title
-             :content content}))
+      [title content]
+      (let [tile-state (atom {:child-tile-state []
+                              :title title
+                              :content content})]
+           tile-state))
 
 (defn calling-component
       []
       [:div
-       [tile (basic-tile-state
-               nil
-               "Basic tile example"
-               (fn []
-                   [:div
-                    [:input {:type "button"
-                             :value "chsk-send! (with reply)"
-                             :on-click (fn []
-                                           (->output! "Button 2 was clicked (will receive reply from server)")
-                                           (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
-                                                       (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]))]])
+       (let [b1 (basic-tile-state
+                  "Basic tile example"
+                  (fn []
+                      [:div
+                       [:input {:type "button"
+                                :value "chsk-send! (with reply)"
+                                :on-click (fn []
+                                              (->output! "Button 2 was clicked (will receive reply from server)")
+                                              (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
+                                                          (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]))]
+            (reset! top-tile-atom b1)
+            (swap! b1 (fn [d] (assoc d :display true)))
+            [tile b1])])
 
 (defn start!
       []
