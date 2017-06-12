86c3941917d985f6633d0ca9e0a24bddc2dce04e
Bill La Forge <laforge49@gmail.com>
2017-06-12 12:58:36 -0400

    list tile

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -31,22 +31,36 @@
                     [:td
                      {:style {:padding "5px"}}
                      (if (some? content)
-                       (content)
+                       (content state)
                        nil)]]]]))))
 
 (defn basic-tile-state
       [title content]
-      (let [tile-state (atom {:child-tile-state []
+      (let [tile-state (atom {:child-tile-states []
                               :title title
                               :content content})]
            tile-state))
 
+(defn list-tile-state
+      [title]
+      (let [tile-state (atom {:child-tile-states []
+                              :title title
+                              :content (fn [state]
+                                           [:dev (count (:child-tile-states @state))])})]
+           tile-state))
+
+(defn add-child-tile
+      [parent-tile-state child-tile-state]
+      (swap! child-tile-state (fn [d] (assoc d :parent-tile-state parent-tile-state)))
+      (swap! parent-tile-state (fn [d] (assoc d :child-tile-states (conj (:child-tile-states d) child-tile-state)))))
+
 (defn calling-component
       []
       [:div
-       (let [b1 (basic-tile-state
-                  "Basic tile example"
-                  (fn []
+       (let [l1 (list-tile-state "Basic tile example")
+             b1 (basic-tile-state
+                  "Test"
+                  (fn [state]
                       [:div
                        [:input {:type "button"
                                 :value "chsk-send! (with reply)"
@@ -54,9 +68,10 @@
                                               (->output! "Button 2 was clicked (will receive reply from server)")
                                               (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
                                                           (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]))]
-            (reset! top-tile-atom b1)
-            (swap! b1 (fn [d] (assoc d :display true)))
-            [tile b1])])
+            (add-child-tile l1 b1)
+            (reset! top-tile-atom l1)
+            (swap! l1 (fn [d] (assoc d :display true)))
+            [tile l1])])
 
 (defn start!
       []
