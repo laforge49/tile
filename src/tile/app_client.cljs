08cfcdd7f66576ba6bf4bcf8ee34b81581fe60fc
Bill La Forge <laforge49@gmail.com>
2017-06-12 11:20:17 -0400

    basic-tile

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -5,45 +5,52 @@
             [taoensso.sente :as sente :refer (cb-success?)]))
 
 (defn tile
-      [title content]
+      [state]
       (fn [state]
-          (let [title (:title @state)
-                content (:content @state)]
-               [:table
-                [:tbody
-                 {:style {:border "5px solid red" :float "left"}}
-                 [:tr
-                  {:style {:background-color "yellow"}}
-                  [:td
-                   {:style {:padding "5px"}}
-                   [:div
-                    [:div
-                     {:style {:float "left"}}
-                     [:strong (str title " ")]]
-                    [:div
-                     {:style {:float "right"}}
-                     "X"]]]]
-                 [:tr
-                  {:style {:background-color "Cornsilk"}}
-                  [:td
-                   {:style {:padding "5px"}}
-                   (content)]]]])))
+          (let [{:keys [display title content]} @state]
+               (if (not display)
+                 nil
+                 [:table
+                  [:tbody
+                   {:style {:border "5px solid red" :float "left"}}
+                   [:tr
+                    {:style {:background-color "yellow"}}
+                    [:td
+                     {:style {:padding "5px"}}
+                     [:div
+                      [:div
+                       {:style {:float "left"}}
+                       [:strong (str title " ")]]
+                      [:div
+                       {:style {:float "right"}}
+                       "X"]]]]
+                   [:tr
+                    {:style {:background-color "Cornsilk"}}
+                    [:td
+                     {:style {:padding "5px"}}
+                     (if (some? content)
+                       (content)
+                       nil)]]]]))))
+
+(defn basic-tile
+      [title content]
+      [tile (atom {:display true
+                   :title title
+                   :content content})])
 
 (defn calling-component
       []
       [:div
-       [tile
-        (atom
-          {:title "Basic tile example"
-           :content (fn []
-                        [:div
-                         [:div "try hitting the button:"]
-                         [:input {:type "button"
-                                  :value "chsk-send! (with reply)"
-                                  :on-click (fn []
-                                                (->output! "Button 2 was clicked (will receive reply from server)")
-                                                (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
-                                                            (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]])})]])
+       [basic-tile
+        "Basic tile example"
+        (fn []
+            [:div
+             [:input {:type "button"
+                      :value "chsk-send! (with reply)"
+                      :on-click (fn []
+                                    (->output! "Button 2 was clicked (will receive reply from server)")
+                                    (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
+                                                (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]])]])
 
 (defn start!
       []
