b0f3624c1a51777ecc90684a9ab28e33db225cdf
Bill La Forge <laforge49@gmail.com>
2017-06-12 10:11:59 -0400

    hard-coded tile form

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -6,16 +6,32 @@
 
 (defn calling-component
       []
-      [:div
-       [:h1 "Tile example"]
-       [:p "try hitting the button:"]
-       [:p
-        [:input {:type "button"
-                 :value "chsk-send! (with reply)"
-                 :on-click (fn []
-                               (->output! "Button 2 was clicked (will receive reply from server)")
-                               (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
-                                           (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]])
+      [:table
+       [:tbody
+        {:style {:border "5px solid red" :float "left"}}
+        [:tr
+         {:style {:background-color "yellow"}}
+         [:td
+          {:style {:padding "5px"}}
+          [:div
+          [:div
+           {:style {:float "left"}}
+           [:strong "Tile example"]]
+          [:div
+           {:style {:float "right"}}
+           "X"]]]]
+        [:tr
+         {:style {:background-color "Cornsilk"}}
+         [:td
+          {:style {:padding "5px"}}
+          [:p "try hitting the button:"]
+          [:p
+           [:input {:type "button"
+                    :value "chsk-send! (with reply)"
+                    :on-click (fn []
+                                  (->output! "Button 2 was clicked (will receive reply from server)")
+                                  (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
+                                              (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]]]]])
 
 (defn start!
       []
