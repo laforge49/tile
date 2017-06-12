1aaa08307ccc1c67203cde8350d98ab8e88b1f31
Bill La Forge <laforge49@gmail.com>
2017-06-12 08:22:34 -0400

    simplified UI

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -8,38 +8,14 @@
       []
       [:div
        [:h1 "Tile example"]
-       [:p [:strong "Step 1: "] " try hitting the buttons:"]
+       [:p "try hitting the button:"]
        [:p
-        [:input {:type "button"
-                 :value "chsk-send! (w/o reply)"
-                 :on-click (fn []
-                               (->output! "Button 1 was clicked (won't receive any reply from server)")
-                               (chsk-send! [:example/button1 {:had-a-callback? "nope"}]))}]
         [:input {:type "button"
                  :value "chsk-send! (with reply)"
                  :on-click (fn []
                                (->output! "Button 2 was clicked (will receive reply from server)")
                                (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
-                                           (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]
-       [:p
-        [:input {:type "button"
-                 :value "Test rapid server>user async pushes"
-                 :on-click (fn []
-                               (->output! "Button 3 was clicked (will ask server to test rapid async push)")
-                               (chsk-send! [:example/test-rapid-push]))}]]
-       [:p
-        [:input {:type "button"
-                 :value "Disconnect"
-                 :on-click (fn []
-                               (->output! "Disconnecting")
-                               (sente/chsk-disconnect! chsk))}]
-        [:input {:type "button"
-                 :value "Reconnect"
-                 :on-click (fn []
-                               (->output! "Reconnecting")
-                               (sente/chsk-reconnect! chsk))}]]
-
-       ])
+                                           (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]])
 
 (defn start!
       []
