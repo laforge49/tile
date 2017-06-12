cee90c669dab5422b68b680f122a4f4c2d1ac31e
Bill La Forge <laforge49@gmail.com>
2017-06-11 23:29:39 -0400

    moved reagent code to app-client

--- src/tile/client.cljs
+++ src/tile/client.cljs
@@ -3,7 +3,6 @@
   {:author "Peter Taoussanis (@ptaoussanis)"}
 
   (:require
-    [reagent.core :as reagent :refer [atom]]
     [clojure.string :as str]
     [cljs.core.async :as async :refer (<! >! put! chan)]
     [taoensso.encore :as encore :refer-macros (have have?)]
@@ -98,108 +97,11 @@
               (sente/start-client-chsk-router!
                 ch-chsk event-msg-handler)))
 
-(defn calling-component
-      []
-      [:div
-       [:h1 "Sente reference example"]
-       [:p "An Ajax/WebSocket" [:strong " (random choice!)"] " has been configured for this example"]
-       [:hr]
-       [:p [:strong "Step 1: "] " try hitting the buttons:"]
-       [:p
-        [:input {:type "button"
-                 :value "chsk-send! (w/o reply)"
-                 :on-click (fn []
-                               (->output! "Button 1 was clicked (won't receive any reply from server)")
-                               (chsk-send! [:example/button1 {:had-a-callback? "nope"}]))}]
-        [:input {:type "button"
-                 :value "chsk-send! (with reply)"
-                 :on-click (fn []
-                               (->output! "Button 2 was clicked (will receive reply from server)")
-                               (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
-                                           (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]
-       [:p
-        [:input {:type "button"
-                 :value "Test rapid server>user async pushes"
-                 :on-click (fn []
-                               (->output! "Button 3 was clicked (will ask server to test rapid async push)")
-                               (chsk-send! [:example/test-rapid-push]))}]
-        [:input {:type "button"
-                 :value "Toggle server>user async broadcast push loop"
-                 :on-click (fn []
-                               (->output! "Button 4 was clicked (will toggle async broadcast loop)")
-                               (chsk-send! [:example/toggle-broadcast] 5000
-                                           (fn [cb-reply]
-                                               (when (cb-success? cb-reply)
-                                                     (let [loop-enabled? cb-reply]
-                                                          (if loop-enabled?
-                                                            (->output! "Async broadcast loop now enabled")
-                                                            (->output! "Async broadcast loop now disabled")))))))}]]
-       [:p
-        [:input {:type "button"
-                 :value "Disconnect"
-                 :on-click (fn []
-                               (->output! "Disconnecting")
-                               (sente/chsk-disconnect! chsk))}]
-        [:input {:type "button"
-                 :value "Reconnect"
-                 :on-click (fn []
-                               (->output! "Reconnecting")
-                               (sente/chsk-reconnect! chsk))}]]
-
-       ])
-
-(defn calling-component2
-      []
-      [:div
-       [:hr]
-       [:h2 "Step 3: try login with a user-id"]
-       [:p "The server can use this id to send events to *you* specifically."]
-       [:p
-        [:input {:id "input-login"
-                 :type :text
-                 :placeholder "User-id"}]
-        [:input {:type "button"
-                 :value "Secure login!"
-                 :on-click (fn []
-                               (let [user-id (.-value (.getElementById js/document "input-login"))]
-                                    (if (str/blank? user-id)
-                                      (js/alert "Please enter a user-id first")
-                                      (do
-                                        (->output! "Logging in with user-id %s" user-id)
-
-                                        ;;; Use any login procedure you'd like. Here we'll trigger an Ajax
-                                        ;;; POST request that resets our server-side session. Then we ask
-                                        ;;; our channel socket to reconnect, thereby picking up the new
-                                        ;;; session.
-
-                                        (sente/ajax-lite "/login"
-                                                         {:method :post
-                                                          :headers {:X-CSRF-Token (:csrf-token @chsk-state)}
-                                                          :params {:user-id (str user-id)}}
-
-                                                         (fn [ajax-resp]
-                                                             (->output! "Ajax login response: %s" ajax-resp)
-                                                             (let [login-successful? true ; Your logic here
-                                                                   ]
-                                                                  (if-not login-successful?
-                                                                          (->output! "Login failed")
-                                                                          (do
-                                                                            (->output! "Login successful")
-                                                                            (sente/chsk-reconnect! chsk))))))))))}]]
-       [:hr]
-       [:h2 "Step 4: want to re-randomize Ajax/WebSocket connection type?"]
-       [:p "Hit your browser's reload/refresh button"]
-       ])
-
 ;;;; Init stuff
 
 (defn start!
       []
       (start-router!)
-      (reagent/render-component [calling-component]
-                                (.getElementById js/document "container"))
-      (reagent/render-component [calling-component2]
-                                (.getElementById js/document "container2"))
       )
 
 (defonce _start-once (start!))
