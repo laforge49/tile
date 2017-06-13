b9def2d8a57ca2226e7c76b679598b31d26d614d
Bill La Forge <laforge49@gmail.com>
2017-06-12 20:45:03 -0400

    circular structure issues

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -10,9 +10,11 @@
       [state]
       (fn [state]
           (let [{:keys [parent-tile-state child-tile-states display title content]} @state]
+               (.log js/console (pr-str (count child-tile-states) display title))
                (if (not display)
                  nil
                  [:table
+                  {:style {:float "left"}}
                   [:tbody
                    {:style {:border "5px solid red" :float "left"}}
                    [:tr
@@ -25,7 +27,7 @@
                        [:strong (str title " ")]]
                       [:div
                        {:style {:float "right"}}
-                       "X"]]]]
+                       " X"]]]]
                    [:tr
                     {:style {:background-color "Cornsilk"}}
                     [:td
@@ -38,27 +40,32 @@
       [title content]
       (let [tile-state (atom {:child-tile-states []
                               :title title
-                              :content content})]
+                              :content content
+                              :display false})]
            tile-state))
 
 (defn list-tile-state
       [title]
-      (let [tile-state (atom {:child-tile-states []
-                              :title title
-                              :content (fn [state]
-                                           (reduce
-                                             (fn [v s]
-                                                 (let [d @s
-                                                       t (:title d)
-                                                       checked (:display d)]
-                                                      (conj v [:div
-                                                               [:input {:type      "checkbox"
-                                                                        :checked   (true? checked)
-                                                                        :on-change (fn []
-                                                                                       (swap! s (fn [d] (assoc d :display (not (true? checked))))))}]
-                                                               t])))
-                                             [:dev]
-                                             (:child-tile-states @state)))})]
+      (let [tile-state (atom
+                         {:child-tile-states []
+                          :title title
+                          :content (fn [state]
+                                       (reduce
+                                         (fn [v s]
+                                             (let [d @s
+                                                   t (:title d)
+                                                   checked (:display d)]
+                                                  (conj v [:div
+                                                           [:input {:type "checkbox"
+                                                                    :checked (true? checked)
+                                                                    :on-change (fn
+                                                                                 []
+                                                                                 (swap! s (fn [d] (assoc d :display (not (true? checked)))))
+                                                                                 (.log js/console (pr-str (:title @s) (:display @s))))}]
+                                                           t])))
+                                         [:dev]
+                                         (:child-tile-states @state)))
+                          :display false})]
            tile-state))
 
 (defn add-child-tile
@@ -66,30 +73,56 @@
       (swap! child-tile-state (fn [d] (assoc d :parent-tile-state parent-tile-state)))
       (swap! parent-tile-state (fn [d] (assoc d :child-tile-states (conj (:child-tile-states d) child-tile-state)))))
 
+(defn tile-states
+      [tile-state]
+      (if (empty? (:child-tile-states @tile-state))
+        [tile-state]
+        (let [x (reduce
+                  (fn [v s]
+                      (.log js/console (pr-str (:title @s) (count (:child-tile-states @s)) (:display @s)))
+                      (if (not (true? (:display @s)))
+                        v
+                        (conj v (tile-states s))))
+                  [tile-state]
+                  (:child-tile-states @tile-state))]
+             x)))
+
+(defn display-tiles
+      [state]
+      ;(.log js/console (pr-str (count (tile-states state))))
+      (reduce
+        (fn [v s]
+            (.log js/console (pr-str :hey (:title @s)))
+            (let [i [tile s]]
+                 (if (nil? i)
+                   v
+                   (conj v i))))
+        [:div]
+        (tile-states state)))
+
 (defn calling-component
       []
-      [:div
-       (let [l1 (list-tile-state "Basic tile example")
-             b1 (basic-tile-state
-                  "Test"
-                  (fn [state]
-                      [:div
-                       [:input {:type "button"
-                                :value "chsk-send! (with reply)"
-                                :on-click (fn []
-                                              (->output! "Button 2 was clicked (will receive reply from server)")
-                                              (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
-                                                          (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]))
-             b2 (basic-tile-state
-                  "Test2"
-                  (fn [state]
-                      [:div 222]))
-             ]
-            (add-child-tile l1 b1)
-            (add-child-tile l1 b2)
-            (reset! top-tile-atom l1)
-            (swap! l1 (fn [d] (assoc d :display true)))
-            [tile l1])])
+      (let [l1 (list-tile-state "Basic tile example")
+            b1 (basic-tile-state
+                 "Test"
+                 (fn [state]
+                     [:div
+                      [:input {:type "button"
+                               :value "chsk-send! (with reply)"
+                               :on-click (fn []
+                                             (->output! "Button 2 was clicked (will receive reply from server)")
+                                             (chsk-send! [:example/button2 {:had-a-callback? "indeed"}] 5000
+                                                         (fn [cb-reply] (->output! "Callback reply: %s" cb-reply))))}]]))
+            b2 (basic-tile-state
+                 "Test2"
+                 (fn [state]
+                     [:div 222]))
+            ]
+           (add-child-tile l1 b1)
+           (add-child-tile l1 b2)
+           (reset! top-tile-atom l1)
+           (swap! l1 (fn [d] (assoc d :display true)))
+           (display-tiles l1)))
 
 (defn start!
       []
