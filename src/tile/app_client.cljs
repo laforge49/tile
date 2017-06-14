e72f695e98abb71129dfb0891323461d1675a728
Bill La Forge <laforge49@gmail.com>
2017-06-14 12:01:02 -0400

    close-tile

--- src/tile/app_client.cljs
+++ src/tile/app_client.cljs
@@ -7,6 +7,14 @@
 (def top-tile-state-atom (atom nil))
 (def all-tile-states-atom (atom []))
 
+(defn close-tile
+  [tile-ndx]
+  (let [tile-state-atom (nth @all-tile-states-atom tile-ndx)]
+    (if (and
+          (> tile-ndx 0)
+          (:display @tile-state-atom))
+          (swap! tile-state-atom assoc :display false))))
+
 (defn tile
   [state]
   (let [{:keys [parent-tile-state child-tile-ndxes display title content tile-ndx]} @state]
@@ -16,8 +24,8 @@
        {:style {:border "5px solid red" :float "left"}}
        [:tbody
         [:tr
-         {:style {:background-color "yellow"}}
          [:td
+          {:style {:background-color "yellow"}}
           [:div
            [:div
             {:style {:float "left"}}
@@ -29,8 +37,7 @@
             [:input {:disabled (= tile-ndx 0)
                      :type "button"
                      :value "X"
-                     :on-click (fn []
-                                 (swap! state assoc :display false))}]]]]]
+                     :on-click #(close-tile tile-ndx)}]]]]]
         [:tr
          {:style {:background-color "Cornsilk"}}
          [:td
@@ -46,7 +53,6 @@
                                :content content
                                :display false})
         tile-ndx (- (count (swap! all-tile-states-atom conj tile-state-atom)) 1)]
-    #_(.log js/console (pr-str :basic tile-ndx))
     (swap! tile-state-atom assoc :tile-ndx tile-ndx)
     tile-state-atom))
 
@@ -62,10 +68,10 @@
                                             (conj v [:div
                                                      [:input {:type "checkbox"
                                                               :checked (true? (:display @s))
-                                                              :on-change (fn
-                                                                           []
-                                                                           (swap! s assoc :display (not (true? (:display @s))))
-                                                                           #_(.log js/console (pr-str :listitem (:title @s) (:display @s))))}]
+                                                              :on-change (fn []
+                                                                           (if (true? (:display @s))
+                                                                             (close-tile ndx)
+                                                                             (swap! s assoc :display true)))}]
                                                      (:title @s)])))
                                         [:dev]
                                         (:child-tile-ndxes @state)))
@@ -76,7 +82,6 @@
 
 (defn add-child-tile
   [parent-tile-state-atom child-tile-state-atom]
-  #_(.log js/console (pr-str :add (:tile-ndx @parent-tile-state-atom) (:tile-ndx @child-tile-state-atom)))
   (swap! child-tile-state-atom assoc :parent-tile-ndx (:tile-ndx @parent-tile-state-atom))
   (swap! parent-tile-state-atom (fn [d] (assoc d :child-tile-ndxes (conj (:child-tile-ndxes d) (:tile-ndx @child-tile-state-atom))))))
 
@@ -86,22 +91,16 @@
     [tile-state]
     (let [x (reduce
               (fn [v ndx]
-                #_(.log js/console (pr-str :ndx ndx))
                 (let [s (nth @all-tile-states-atom ndx)]
-                  #_(.log js/console (pr-str :tile-state-n (:title @s) (count (:child-tile-ndxes @s)) (:display @s)))
-                  ;(if (not (true? (:display @s)))
-                   ; v
-                    (into v (tile-states s))));)
+                  (into v (tile-states s))));)
               [tile-state]
               (:child-tile-ndxes @tile-state))]
       x)))
 
 (defn display-tiles
   [state]
-  #_(.log js/console (pr-str (count (tile-states state))))
   (reduce
     (fn [v s]
-      #_(.log js/console (pr-str :hey (:title @s)))
       (let [i [tile s]]
         (if (nil? i)
           v
